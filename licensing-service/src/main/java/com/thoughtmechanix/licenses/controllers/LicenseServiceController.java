42a67d600c9c991e79102671f3b98b0ba97d6923
carnellj <john.carnell@inin.com>
2018-02-15 21:12:34 -0500

    Fixed readme.md

--- licensing-service/src/main/java/com/thoughtmechanix/licenses/controllers/LicenseServiceController.java
+++ licensing-service/src/main/java/com/thoughtmechanix/licenses/controllers/LicenseServiceController.java
@@ -5,7 +5,6 @@ import com.thoughtmechanix.licenses.services.LicenseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.PathVariable;
